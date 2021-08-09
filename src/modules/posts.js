d6f1641085e5f6cdd32fdb4b5162109770c1e4f1
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2021-08-09 18:31:23 +0900

    Add CLEAR_POST action for use experience

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -9,8 +9,11 @@ const GET_POST = 'posts/GET_POST';
 const GET_POST_SUCCESS = 'posts/GET_POST_SUCCESS';
 const GET_POST_ERROR = 'posts/GET_POST_ERROR';
 
+const CLEAR_POST = 'posts/CLEAR_POST';
+
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const clearPost = () => ({ type: CLEAR_POST });
 
 // thunk 함수에서도 파라미터를 받아와서 사용 할 수 있습니다.
 
@@ -18,17 +21,24 @@ const initialState = {
   posts: reducerUtils.initial(),
   post: reducerUtils.initial(),
 };
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+      return getPostsReducer(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action);
+      return getPostReducer(state, action);
+    case CLEAR_POST:
+      return {
+        ...state,
+        post: reducerUtils.initial(),
+      };
     default:
       return state;
   }
